bcd635597c999c2756fe2d7920b63aa6fb34cb1d
edocai <edocaai@hotmail.com>
2023-02-26 16:50:48 +0000

    todos model implemented using zod

---
+++ src/api/todos/todos.model.ts
@@ -0,0 +1,13 @@
+//scheme validation library build in TS
+import * as z from 'zod';
+
+const Todo = z.object({
+  content: z.string().min(1),
+  done: z.boolean(),
+});
+
+//makes an interface
+type Todo = z.infer<typeof Todo>;
+
+export default Todo;
+
