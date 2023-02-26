e5bd40a93d09725071f17c58f22f62880a84d76a
edocai <edocaai@hotmail.com>
2023-02-26 18:55:43 +0000

    implemented todos router and mounted router

---
+++ src/api/todos/todos.router.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import Todo from './todos.model';
+
+const router = Router();
+
+router.get<{}, Todo[]>('/', (req, res) => {
+  res.json([{
+    content: 'hello',
+    done: true,
+  }]);
+});
+
+export default router;
