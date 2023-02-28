9d39c0531493b0c6b1baa879ea161891b61e8921
edocai <edocaai@hotmail.com>
2023-02-28 14:18:38 +0000

    changing test to async to experiment

--- src/api/todos/todos.test.ts
+++ src/api/todos/todos.test.ts
@@ -3,20 +3,14 @@ import request from 'supertest';
 import app from '../../app';
 
 describe('GET /api/todos', () => {
-  it('responds with an array of todos', (done) => {
+  it('responds with an array of todos', async () => 
     request(app)
       .get('/api/todos')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(
-        200,
-        [
-          {
-            content: 'hello',
-            done: true,
-          },
-        ],
-        done,
-      );
-  });
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toHaveProperty('length');
+        expect(response.body.length).toBe(1);
+      }));
 });
